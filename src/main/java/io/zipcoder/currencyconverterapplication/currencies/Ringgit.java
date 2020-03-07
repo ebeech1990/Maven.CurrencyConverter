bd79f2ac2e4907d7994b8eb5b80db4fceaa4876b
Emma Beech <ebeech@zipcodes-MacBook-Pro.local>
2020-03-07 13:10:18 -0500

    done

--- src/main/java/io/zipcoder/currencyconverterapplication/currencies/Ringgit.java
+++ src/main/java/io/zipcoder/currencyconverterapplication/currencies/Ringgit.java
@@ -1,6 +1,14 @@
 package io.zipcoder.currencyconverterapplication.currencies;
 
 import io.zipcoder.currencyconverterapplication.ConvertableCurrency;
+import io.zipcoder.currencyconverterapplication.CurrencyConverter;
+import io.zipcoder.currencyconverterapplication.CurrencyType;
 
 public class Ringgit implements ConvertableCurrency {
+
+    CurrencyType type = CurrencyType.RINGGIT;
+
+    public CurrencyType getType() {
+        return type;
+    }
 }
